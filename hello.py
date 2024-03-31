6a934127ca12867c4d3c9c7adedff61dcd7353aa
lemartien25000 <fsof2007@gmail.com>
2024-03-31 15:03:49 +0100

    tweaked 500.html

--- hello.py
+++ hello.py
@@ -25,7 +25,7 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-# internal server error
+# internal server error thing....
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
